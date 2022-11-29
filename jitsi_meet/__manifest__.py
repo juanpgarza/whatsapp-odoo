449dae24ddf96ac36f62e068f517def45097c5c0
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-29 09:57:35 -0300

    [12.0][ADD] jitsi_meet

---
+++ jitsi_meet/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Jitsi Meet",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Misc",
+    "website": "https://github.com/OpenNovaSoft/misc-addons",
+    "author": "openNova",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "data/jitsi_meet.xml",
+        'security/ir.model.access.csv',
+        ],
+    "installable": True,
+}
---
+++ jitsi_meet/models/jitsi_meet.py
@@ -0,0 +1,27 @@
+# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models, api
+from random import choice
+
+def create_hash():
+    size = 32
+    values = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    h = ''
+    h = h.join([choice(values) for i in range(size)])
+    return h
+
+class JitsiMeet(models.Model):
+    _name = 'jitsi.meet'
+    _description = 'Jitsi meet'
+
+    name = fields.Char(string="name", default="jitsi meet")
+    jitsi_link = fields.Char(string="Link Jitsi", readonly=True)
+    hash = fields.Char('Hash')
+
+    @api.model
+    def create(self, vals):
+        vals['hash'] = create_hash()
+        config_url =  self.env['ir.config_parameter'].sudo().get_param('jitsi_meet.jitsi_event_url',default='https://meet.jit.si/')
+        vals['jitsi_link'] = config_url + vals['hash']
+        res = super(JitsiMeet, self).create(vals)
+        return res
