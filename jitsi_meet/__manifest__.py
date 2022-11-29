f12a37f5aae38b75b113b1d8d2945511f6ee2ce7
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-29 10:07:02 -0300

    [15.0][MIG] jitsi_meet

--- jitsi_meet/__manifest__.py
+++ jitsi_meet/__manifest__.py
@@ -1,15 +1,17 @@
-# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Jitsi Meet",
     "summary": "",
-    "version": "12.0.1.0.0",
-    "category": "Misc",
-    "website": "https://github.com/OpenNovaSoft/misc-addons",
-    "author": "openNova",
+    "version": "15.0.1.0.0",
+    "category": "Social",
+    "website": "https://github.com/juanpgarza/whatsapp-odoo",
+    "author": "juanpgarza",
     "license": "AGPL-3",
-    "depends": ["base"],
+    "depends": [
+        "base",
+        ],
     "data": [
         "data/jitsi_meet.xml",
         'security/ir.model.access.csv',
--- jitsi_meet/models/jitsi_meet.py
+++ jitsi_meet/models/jitsi_meet.py
@@ -1,5 +1,3 @@
-# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models, api
 from random import choice
 
