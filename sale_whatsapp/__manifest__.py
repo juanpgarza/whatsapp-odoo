430882628574277e9336601b335b1f5fae541c22
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-07-03 12:30:12 +0000

    [MIG] sale_whatsapp: Migration to 18.0

--- sale_whatsapp/__manifest__.py
+++ sale_whatsapp/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale whatsapp integrations",
     "summary": "Integrate whatsapp with Sale App",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Social",
     "website": "https://github.com/juanpgarza/whatsapp-odoo",
     "author": "juanpgarza",
@@ -21,5 +21,5 @@
         'security/ir.model.access.csv',
         ],
     "development_status": "Production/Stable",
-    "installable": False,
+    "installable": True,
 }
